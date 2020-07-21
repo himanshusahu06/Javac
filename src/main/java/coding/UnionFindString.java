491546ba53c879ec26bd75df4746651df5838d0e
Himanshu Sahu <himanshusahu24@gmail.com>
2020-07-21 17:46:38 +0000

    Update UnionFindString.java

--- src/main/java/coding/UnionFindString.java
+++ src/main/java/coding/UnionFindString.java
@@ -1,18 +1,18 @@
-public class UF {
-    Map<String, String> id = new HashMap<String, String>();
+class UnionFind {
+    Map<String, String> id = new HashMap<>();
+
+    public boolean connected(String u, String v) {
+        return find(u).equals(find(v));
+    }
 
     public void union(String u, String v) {
         id.put(find(u), find(v));
     }
 
-    private String find(String v) {
-        while(!v.equals(id.computeIfAbsent(v, key -> key))) {
-            v = id.get(v);
+    private String find(String s) {
+        while(!s.equals(id.getOrDefault(s, s))) {
+            s = id.get(s);
         }
-        return v;
-    }
-
-    public boolean connected(String u, String v) {
-        return find(u).equals(find(v));
+        return s;
     }
 }
